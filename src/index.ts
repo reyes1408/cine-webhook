0b9207a82f7c2c4178d7a37784d94ac0eff5ef81
Daniel Reyes <danielreher@outlook.com>
2024-06-20 01:02:28 -0600

    se agregó sse

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import app from "./app"; //Contiene el servidor de express
+import app from "./app"; // Contiene el servidor de express
 import { sequelize } from "./config/config.mysql";
 import { createServer } from "http";
 import WebSocket, { Server } from "ws";
@@ -6,7 +6,8 @@ import { Request, Response } from "express";
 
 // let asientosVendidos = 0;
 let usuariosConectados = 0;
-let respuestasPendientes: Response[] = [];
+let sseClients: Response[] = [];
+let userSseClients: Response[] = [];
 
 // Inicializar sillas
 let sillas = [
@@ -28,66 +29,98 @@ let sillas = [
 const server = createServer(app);
 
 // Creamos el servidor WebSocket y se asocia con el servidor HTTP
-const wss = new Server({ server })
+const wss = new Server({ server });
 
-wss.on('connection', (ws: WebSocket) => {
-  console.log('Nuevo Cliente');
+wss.on("connection", (ws: WebSocket) => {
+  console.log("Nuevo Cliente");
   usuariosConectados += 1;
-  // ws.send(`Usuario: ${usuariosConectados}`)
+  enviarUserSSEClientes();
 
   // Se envia el estado de las sillas al nuevo cliente.
-  ws.send(JSON.stringify({ type: 'init', sillas }))
+  ws.send(JSON.stringify({ type: "init", sillas }));
 
-  ws.on('message', (message: string) => {
+  ws.on("message", (message: string) => {
     const parsedMessage = JSON.parse(message);
-    console.log('Received:', parsedMessage);
+    console.log("Received:", parsedMessage);
 
-    if (parsedMessage.type === 'comprar') {
+    if (parsedMessage.type === "comprar") {
       const { id } = parsedMessage;
-      const silla = sillas.find(silla => silla.id === id);
+      const silla = sillas.find((silla) => silla.id === id);
       if (silla && !silla.comprado) {
         silla.comprado = true;
 
         // Enviar actualización a todos los clientes conectados
-        wss.clients.forEach(client => {
+        wss.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: 'update', sillas }));
+            client.send(JSON.stringify({ type: "update", sillas }));
           }
         });
 
-        // responder a los clientes que esperan una nueva notificacion
-        responderClientes();
+        // Enviar actualización a los clientes SSE
+        enviarSSEClientes();
       }
     }
   });
 
-  ws.on('close', () => {
-    console.log('Client disconnected');
+  ws.on("close", () => {
+    console.log("Client disconnected");
     usuariosConectados -= 1;
+    enviarUserSSEClientes();
   });
 
-  ws.on('error', (error) => {
+  ws.on("error", (error) => {
     usuariosConectados -= 1;
-    console.error('WebSocket error:', error);
+    enviarUserSSEClientes();
+    console.error("WebSocket error:", error);
+  });
+});
+
+// --- SSE (Clientes en linea) ---
+app.get("/api/clientOnline", (req: Request, res: Response) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  userSseClients.push(res);
+
+  req.on("close", () => {
+    userSseClients = userSseClients.filter((client) => client !== res);
   });
 
+  // Enviar la cantidad inicial de usuarios conectados al nuevo cliente
+  res.write(`data: ${JSON.stringify({ usuariosConectados })}\n\n`);
 });
 
-app.get('/api/userOnline', (_req, res) => {
-  res.json({ usuariosConectados });
+function enviarUserSSEClientes() {
+  userSseClients.forEach((client) => {
+    client.write(`data: ${JSON.stringify({ usuariosConectados })}\n\n`);
+    console.log({ data: usuariosConectados });
+  });
+}
+
+// --- SSE (Sillas vendidas) ---
+app.get("/api/sillasSSE", (req: Request, res: Response) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  sseClients.push(res);
+
+  req.on("close", () => {
+    sseClients = sseClients.filter((client) => client !== res);
+  });
+
+  // Enviar la cantidad inicial de sillas vendidas al nuevo cliente
+  const sillasVendidas = sillas.filter((silla) => silla.comprado).length;
+  res.write(`data: ${JSON.stringify({ sillasVendidas })}\n\n`);
 });
 
-app.get('/api/sillasVendidas', (_req: Request, res: Response) => {
-  respuestasPendientes.push(res)
-});
-
-function responderClientes() {
-  for (const res of respuestasPendientes) {
-    const sillasVendidas = sillas.filter(silla => silla.comprado).length;
-    res.status(200).json({ sillasVendidas });
-  }
-
-  respuestasPendientes = [];
+function enviarSSEClientes() {
+  const sillasVendidas = sillas.filter((silla) => silla.comprado).length;
+  sseClients.forEach((client) => {
+    client.write(`data: ${JSON.stringify({ sillasVendidas })}\n\n`);
+    console.log({ data: sillasVendidas });
+  });
 }
 
 const PORT = 3000;
@@ -96,11 +129,11 @@ const PORT = 3000;
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    console.log('Database connected');
+    console.log("Database connected");
     server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 })();
